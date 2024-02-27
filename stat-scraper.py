a22d2c5ad824019a98205bcbcc8ff50e19296216
em2063 <ethanmcgrogan1@gmail.com>
2024-02-27 17:38:11 +0000

    gets player stats table

--- stat-scraper.py
+++ stat-scraper.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd 
 from bs4 import BeautifulSoup
+import time
 
 standings_url = "https://fbref.com/en/comps/40/Scottish-Premiership-Stats"
 data = requests.get(standings_url)
@@ -11,4 +12,8 @@ links = [l.get("href") for l in standings_table.find_all('a')]
 links = [l for l in links if '/squads/' in l]
 team_urls = [f"https://fbref.com/{l}" for l in links]
 
-print(team_urls)
+team_url = team_urls[0]
+
+data = requests.get(team_url)
+
+players = pd.read_html(data.text, match="Standard Stats")[0]
