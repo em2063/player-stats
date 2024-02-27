425394ecb4a6152a57eb119c4577992d9f9aee3c
em2063 <ethanmcgrogan1@gmail.com>
2024-02-27 18:08:39 +0000

    adds loop for every team player stats

--- stat-scraper.py
+++ stat-scraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import time
 
+all_players = []
 standings_url = "https://fbref.com/en/comps/40/Scottish-Premiership-Stats"
 data = requests.get(standings_url)
 soup = BeautifulSoup(data.text, 'html.parser')
@@ -12,8 +13,18 @@ links = [l.get("href") for l in standings_table.find_all('a')]
 links = [l for l in links if '/squads/' in l]
 team_urls = [f"https://fbref.com/{l}" for l in links]
 
-team_url = team_urls[0]
+for team_url in team_urls:
+    team_name = team_url.split('/')[-1].replace("-Stats", "").replace("-", " ")
+    data = requests.get(team_url)
+    players = pd.read_html(data.text, match="Standard Stats")[0]
+    remove_cols = ['Per 90 Minutes']
+    players = players.drop(columns=remove_cols)
+    players["Team"] = team_name
+    all_players.append(players)
+    time.sleep(2)
 
-data = requests.get(team_url)
+player_df = pd.concat(all_players)
+print("creating csv...")
+player_df.to_csv("players.csv")
 
-players = pd.read_html(data.text, match="Standard Stats")[0]
+    
