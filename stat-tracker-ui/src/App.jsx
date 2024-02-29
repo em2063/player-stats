de2e35af3d3730a63772470edb908e564d1bf96a
em2063 <ethanmcgrogan1@gmail.com>
2024-02-29 14:49:53 +0000

    start of home

--- stat-tracker-ui/src/App.jsx
+++ stat-tracker-ui/src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import Home from "./components/Home.jsx";
+import Nav from "./components/nav.jsx";
+import "./styles/styles.css";
+
+const App = () => {
   return (
     <>
-      <p>Hello</p>
+      <header>
+        <Nav></Nav>
+      </header>
+      <Home></Home>
     </>
   );
-}
+};
 
 export default App;
---
+++ stat-tracker-ui/src/components/nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <a href="#">Teams</a>
+        <a href="#">StatHoose</a>
+        <a href="#">Players</a>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
