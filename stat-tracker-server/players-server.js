05018395ca2ef8693247505e00e2a69ed72db644
em2063 <ethanmcgrogan1@gmail.com>
2024-03-02 20:12:25 +0000

    init server to mysql

---
+++ stat-tracker-server/players-server.js
@@ -0,0 +1,40 @@
+const mysql = require("mysql");
+const { json } = require("react-router-dom");
+
+// create connection to mysql
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Ethan1875!",
+  database: "players",
+});
+
+// Connect to the mysql server
+connection.connect((err) => {
+  if (err) {
+    console.error("can't connect:", err.stack);
+    return;
+  }
+  console.log("Connected");
+
+  // test query
+  connection.query("SELECT * FROM players", (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err.stack);
+      return;
+    }
+
+    // test results
+    console.log("Player data:", results);
+
+    // Close connection
+    connection.end((err) => {
+      if (err) {
+        console.error("Error closing connection:", err.stack);
+        return;
+      }
+
+      console.log("Connection closed");
+    });
+  });
+});
