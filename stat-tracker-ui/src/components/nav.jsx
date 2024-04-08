9fa8efabf991a6a33b1f4b030a9c2dd3618c6772
em2063 <em2063@hw.ac.uk>
2024-04-08 21:28:42 +0100

    adds state management for sidebar

--- stat-tracker-ui/src/components/nav.jsx
+++ stat-tracker-ui/src/components/nav.jsx
@@ -1,15 +1,60 @@
 import "../styles/styles.css";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 const Nav = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const moveSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
   return (
     <>
       <nav className="nav">
-        <Link to="/teams">Teams</Link>
-        <Link className="nav-brand" to="/">
-          StatPadder
-        </Link>
-        <Link to="/players">Players</Link>
+        <ul>
+          <li className="hideOnMobile">
+            <Link to="/teams">Teams</Link>
+          </li>
+          <li>
+            <Link className="nav-brand" to="/">
+              StatPadder
+            </Link>
+          </li>
+          <li className="hideOnMobile">
+            <Link to="/players">Players</Link>
+          </li>
+          <li onClick={() => moveSidebar()} className="menu-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
+          </li>
+        </ul>
+        {sidebar && (
+          <ul id="sidebar">
+            <li onClick={() => moveSidebar()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 -960 960 960"
+                width="24"
+              >
+                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+              </svg>
+            </li>
+            <li>
+              <Link to="/teams">Teams</Link>
+            </li>
+            <li>
+              <Link to="/players">Players</Link>
+            </li>
+          </ul>
+        )}
       </nav>
     </>
   );
